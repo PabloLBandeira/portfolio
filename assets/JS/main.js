a8683409c361d5139de65911bfc87db89a5fb799
PabloBandeira <pablo.bandeira@ymail.com>
2025-03-11 15:39:44 -0300

    wip: updating profile.json and main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,47 @@
+function updateProfileInfo(profileData) {
+  const photo = document.getElementById('profile.photo')
+  photo.src=profileData.photo
+  photo.alt=profileData.name
 
+  const name = document.getElementById('profile.name')
+  name.innerText = profileData.name
+
+  const job = document.getElementById('profile.job')
+  job.innerHTML = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.innerText = profileData.location
+
+  const phone = document.getElementById('profile.phone')
+  phone.innerText = profileData.phone
+
+  const cleanedPhone = profileData.phone.replace(/\D/g, '')
+  const whatsappLink = `https://wa.me/55${cleanedPhone}`
+  phone.href = whatsappLink
+
+  const email = document.getElementById('profile.email')
+  email.innerText = profileData.email
+  email.href = `mailto:${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById('profile.skills.softSkills')
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById('profile.skills.hardSkills')
+  hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => <img src="${skill.logo}" alt="${}skill.name"></img>)
+}
+ 
+ 
+ 
+ 
  (async () => {
   const profileData = await fetchProfileData()
-  console.log(profileData)
+  updateProfileInfo(profileData)
+  updateSoftSkills(profileData)
+  updateHardSkills(profileData)
  })()
 
 
